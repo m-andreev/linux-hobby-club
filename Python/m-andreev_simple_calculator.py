b9248609639d978138e92f9d5442570bbb34108b
mario on debian lab <m.andreev@abv.bg>
2023-04-06 18:46:17 -0400

    Multiple operations implemented

--- Python/m-andreev_simple_calculator.py
+++ Python/m-andreev_simple_calculator.py
@@ -2,5 +2,14 @@
 
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
-print(f'The result is: {a+b}')
+c = input("Enter operation: ")
+if c == '+':
+    result = a+b
+elif c == '-':
+    result = a-b
+elif c == '*':
+    result = a*b
+elif c == '/':
+    result = a/b
+print(f'The result is: {result}')
 
